8692f24a8b972253fcc80046cb4d057c287251bb
Jessica Chung <jchung722@gmail.com>
2019-09-06 11:25:29 -0500

    update sqlite database

--- lib/Database.rb
+++ lib/Database.rb
@@ -1,12 +1,14 @@
 require "sequel"
 
 module Database
-    SEQUEL = Sequel.sqlite
-
-    SEQUEL.create_table :tasks do 
-        primary_key :id
-        String :title
-        String :description
-        Boolean :is_complete
+    DB = Sequel.connect("sqlite://development.db")
+    
+    unless DB.table_exists?(:tasks)
+        DB.create_table :tasks do 
+            primary_key :id
+            String :title
+            String :description
+            Boolean :is_complete
+        end
     end
 end
